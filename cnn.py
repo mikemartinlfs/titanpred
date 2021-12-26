0a582f252843195953abebb67dc4372770457125
mikemartinlfs <mikemartinklfs@gmail.com>
2021-12-26 11:21:07 -0500

    update on 26 Dec 2021

--- cnn.py
+++ cnn.py
@@ -9,3 +9,80 @@ from torch.autograd import Variable
 from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Module, Softmax, BatchNorm2d, Dropout
 from torch.optim import Adam, SGD
 
+
+class Net(Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.cnn_layers=Sequential(
+            Conv2d(1, 6, kernel_size=3, stride=1, padding=1),
+            BatchNorm2d(4),
+            ReLU(inplace=True),
+            MaxPool2d(kernel_size=2,stride=2),
+            Conv2d(6,6,kernel_size=3,stride=1,padding=1),
+            BatchNorm2d(4),
+            ReLU(inplace=True),
+            MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.linear_layers=Sequential(
+            Linear(10, 2)
+        )
+
+    def forward(self,x):
+        x=self.cnn_layers(x)
+        x=x.view(x.size(0),-1)
+        x=self.linear_layers(x)
+        return x
+
+model=Net()
+optimizer=Adam(model.parameters(),lr=0.07)
+criterion=CrossEntropyLoss()
+if torch.cuda.is_available():
+    model=model.cuda()
+    criterion=criterion.cuda()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def train(epoch):
+    model.train()
+    tr_loss=0
+    #get the training set
+    x_train,y_train=Variable(train_x),Variable(train_y) #need to set up train_y
+
+    #get validation set
+    x_val,y_val=Variable(val_x),Variable(val_y) #need to set this up
+
+    #set data types to use GPU if available
+    if torch.cuda.is_available():
+        x_train=x_train.cuda()
+        y_train=y_train.cuda()
+        x_val=x_val.cuda()
+        y_val-y_val.cuda()
+
+    #clearing gradients from model parameters
+    optimizer.zero_grad()
+
+    #prediction for training/validation set
+    output_train=model(x_train)
+    output_val=model(x_val)
+
+    #Compute training and validation loss
+    loss_train=criterion(output_train,y_train)
+    loss_val=criterion(output_val,y_val)
+    train_losses.append(loss_train)
+    val_losses.append(loss_val)
+
+    #compute updated weights of model parameters
+    loss_train.backward()
+    optimizer.step()
+    tr_loss=loss_train.item()
+    if epoch%2==0:
+        #print validation loss if so
+        print('Epoch : ',epoch+1, '\t', 'loss :', loss_val)
+
+n_epochs=25
+train_losses=[]
+val_losses=[]
+
+for epoch in range(n_epochs):
+    train(epoch)
--- main.py
+++ main.py
@@ -2,74 +2,79 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import torch
+from sklearn.preprocessing import LabelEncoder
+from torch.utils.data import DataLoader,Dataset
 
 # %%
-# 1: Code each criteria, #2 Put each into a vector
-def titanproc(filepath):
-    df = pd.read_csv(filepath)
 
-    try:
-        survived = list(df['Survived'])
-        survived2 = ["Y" if x == 1 else "N" for x in survived]
-        survived = pd.get_dummies(survived2)
-    except:
-        survived = []
 
-    df = df[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Cabin', 'Embarked']].copy()
-    sex = list(pd.get_dummies(df['Sex'])['male'])
-    pclass = list(df['Pclass'] / max(df['Pclass']))
-    age = list(df['Age'] / max(df['Age']))
-    agezero = -df['Age'].isna()
-    age2 = []
-    for i in range(len(agezero)):
-        if agezero[i]:
-            age2.append(age[i])
+class titanicdata(Dataset):
+    def __init__(self,csvpath,mode='train'):
+        self.mode=mode
+        df=pd.read_csv(csvpath)
+        if self.mode=='train':
+            self.oup = list(df['Survived'])
+            print(list(df['Survived']))
+            print(self.oup)
+        df = df[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Cabin', 'Embarked']].copy()
+        le=LabelEncoder()
+        sex = list(pd.get_dummies(df['Sex'])['male'])
+        pclass = list(df['Pclass'] / max(df['Pclass']))
+        age = list(df['Age'] / max(df['Age']))
+        agezero = -df['Age'].isna()
+        age2 = []
+        for i in range(len(agezero)):
+            if agezero[i]:
+                age2.append(age[i])
+            else:
+                age2.append(0)
+        age = age2
+        sibsp = list(df['SibSp'] / max(df['SibSp']))
+        parch = list(df['Parch'] / max(df['Parch']))
+        fare = list(df['Fare'] / max(df['Fare']))
+
+        df['Cabin'] = df['Cabin'].fillna(0)
+        numcabs = [(str(x).count(' ') + 1) / 4 if x != 0 else x - x for x in list(df['Cabin'])]
+
+        emb = pd.get_dummies(df['Embarked'])
+        embarked = list((emb['C'] + (2 * emb['Q']) + (3 * emb['S'])) / 3)
+        cabin = [x.split()[0] if str(x).count(' ') > 0 else x for x in
+                 list(df['Cabin'])]  # Get the first cabin listed if more than 1, otherwise return the cabin or 0
+        firstcab = [str(x)[0] for x in cabin]  # Get the cabin letter - or 0 for nan values
+        fc = pd.get_dummies(firstcab)  # convert to numeric values
+
+        n = len(fc)
+        cabletter = [0] * n
+        weight = 0
+        for i in fc:
+            val = str(i)
+            cabletter += fc[i] * weight
+            weight += 1
+
+        cabletter = cabletter / 10
+
+        cabnum = [int(str(x)[1:]) / 148 if len(str(x)) > 1 else 0 for x in
+                  cabin]
+        normed = pd.DataFrame(list(zip(age, sex, sibsp, parch, fare, pclass, cabletter, cabnum, numcabs)))
+        normed=normed.values.reshape(normed.shape[0],3,3)
+        self.inp = normed
+
+    def __len__(self):
+        return len(self.inp)
+
+    def __getitem__(self, idx):
+        if self.mode=='train':
+            inpt=torch.Tensor(self.inp)[idx]
+            oupt=torch.Tensor(self.oup)[idx]
+            return { 'inp': inpt,
+                     'oup': oupt,}
         else:
-            age2.append(0)
-    age = age2
-    sibsp = list(df['SibSp'] / max(df['SibSp']))
-    parch = list(df['Parch'] / max(df['Parch']))
-    fare = list(df['Fare'] / max(df['Fare']))
+            inpt=torch.Tensor(self.inp)[idx]
+            return {'inp': inpt}
 
-    df['Cabin'] = df['Cabin'].fillna(0)
-    numcabs = [(str(x).count(' ') + 1) / 4 if x != 0 else x - x for x in list(df['Cabin'])]
-
-    emb = pd.get_dummies(df['Embarked'])
-    embarked = list((emb['C'] + (2 * emb['Q']) + (3 * emb['S'])) / 3)
-    cabin = [x.split()[0] if str(x).count(' ') > 0 else x for x in
-             list(df['Cabin'])]  # Get the first cabin listed if more than 1, otherwise return the cabin or 0
-    firstcab = [str(x)[0] for x in cabin]  # Get the cabin letter - or 0 for nan values
-    fc = pd.get_dummies(firstcab)  # convert to numeric values
-
-    n = len(fc)
-    cabletter = [0] * n
-    weight = 0
-    for i in fc:
-        val = str(i)
-        cabletter += fc[i] * weight
-        weight += 1
-
-    cabletter = cabletter / 10
-
-    cabnum = [int(str(x)[1:]) / 1480 if len(str(x)) > 1 else 0 for x in
-              cabin]  # Get and normalize the cab number, using 10*max when dividing to put the cabin number values in the hundredths place
-
-    cabin = cabletter + cabnum  # The tenths place stands for the cabin letter, the hundredths through however far the values to stand for the cabin number
-
-    normed = pd.DataFrame(list(zip(age, sex, sibsp, parch, fare, pclass, cabin, numcabs)),
-                          columns=['Age', 'Sex', 'SibSP', 'Parch', 'Fare', 'Pclass', 'Cabin', '#Cabins'])
-    return normed, survived
-
-
-train, survived = titanproc('/Users/mikemartinlfs/Downloads/train.csv')
-test, na = titanproc('/Users/mikemartinlfs/Downloads/test.csv')
-
-# %%
-# 3. Using a CNN, predict end result
-
-X_train, X_test, y_train, y_test = train_test_split(train, survived, test_size=0.33, random_state=42)
-
-
-
-
-
+data=titanicdata('/Users/mikemartinlfs/Downloads/train.csv')
+train_x=DataLoader(dataset=data,batch_size=64,shuffle=False)
+dataiter=iter(train_x)
+x,y=dataiter.next()
+print(x)
+print(y)
